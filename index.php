9909f299a92f58edb2f14c09e54dcd2b98f5bceb
opah princewill <princewillopah@gmail.com>
2023-05-15 09:33:05 +0100

    from home 15/05/23

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	$title = 'Home';
+	$title = 'Get the best shipping solution at the best price';
 	$page ="home";
    include "includes/header.php";
 ?>
@@ -36,7 +36,7 @@
                                             Solution
                                         </h1>
                                         <div class="button-area-banner">
-                                            <a href="#" class="rts-btn btn-primary">Get Started</a>
+                                            <a href="contact.php" class="rts-btn btn-primary">Get Started</a>
                                             <!-- call now information -->
                                             <div class="call-area">
                                                 <div class="icon">
@@ -78,7 +78,7 @@
                                             Solution
                                         </h1>
                                         <div class="button-area-banner">
-                                            <a href="#" class="rts-btn btn-primary">Get Started</a>
+                                            <a href="contact.php" class="rts-btn btn-primary">Get Started</a>
                                             <!-- call now information -->
                                             <div class="call-area">
                                                 <div class="icon">
@@ -120,7 +120,7 @@
                                             Solution
                                         </h1>
                                         <div class="button-area-banner">
-                                            <a href="#" class="rts-btn btn-primary">Get Started</a>
+                                            <a href="contact.php" class="rts-btn btn-primary">Get Started</a>
                                             <!-- call now information -->
                                             <div class="call-area">
                                                 <div class="icon">
@@ -152,6 +152,41 @@
 
 
 
+  <!-- rts about us section start -->
+  <div class="rts-about-us-area rts-section-gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="thumbnail-about-six">
+                        <img class="image-lg" src="assets/images/about/12.jpg" alt="about-image">
+                        <img class="image-sm" src="assets/images/about/13.jpg" alt="about-image">
+                    </div>
+                </div>
+                <div class="col-lg-6 pl--65 pl_md--15 pl_sm--15 pt_md--60 pt_sm--80">
+                    <div class="title-left-wrapper-home-two sal-animate mb--30" data-sal="slide-up" data-sal-delay="150" data-sal-duration="800">
+                        <span class="pre">CAMBIO SCHEEPVAART BV features</span>
+                        <h2 class="title mt--10">Our mission <br> is to deliver energy worldwide </h2>
+                    </div>
+                    <div class="about-six-inner-wrapper">
+                        <p class="disc">
+                        CAMBIO SCHEEPVAART BV is a leading company that specializes in the transportation of mineral oils, liquid chemicals, and various liquid products through water and rail. Our services are built upon the pillars of safety, sustainability, and dependable communication. With our skilled team, we handle daily transportation operations through rail, river, canal, and major seaports across Europe. At CAMBIO SCHEEPVAART BV, we boast a state-of-the-art fleet of environmentally friendly double-walled tankers and tank wagons, catering to clients in the chemical and mineral industries.
+                        </p>
+                        <p class="disc">
+                        CAMBIO SCHEEPVAART BV is a privately owned company engaged in the operation of crude oil tankers. Our fleet consists of VLCC (Very Large Crude Carrier) tankers that conduct business globally.
+                        </p>
+                        <p class="disc">We manage a vast network of more than 80 fleets and trains, spanning across France and the Netherlands. Additionally, we provide sub-lease services at the port of Houston, Texas.</p>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts about us section end -->
+
+
+
+
     <!-- rts cta area start -->
     <div class="rts-cta-area rts-section-gap2Bottom">
         <div class="container container-cta-150">
@@ -159,7 +194,7 @@
                 <div class="col-lg-6">
                     <div class="path-cta-left">
                         <h3 class="title">
-                            Do you want to get estimation?
+                            Do you want to get started?
                         </h3>
                         <p class="disc">Get free consultation of your shipping solutions. feel free to call us.</p>
                     </div>
